a0ba605ef59612cc8bde4cffe3bbc8b45e598d42
Nursat <nursatalzanov@gmail.com>
2024-05-06 16:37:57 +0500

    final

--- src/App.js
+++ src/App.js
@@ -3,37 +3,47 @@ import './App.css';
 
 function App() { // отображение 
   const [notes, setNotes] = useState([]);
-  const [newNoteText, setNewNoteText] = useState('');
+  const [newNoteText, setNewNoteText] = useState('');  //notes хранит список заметок, а newNoteText хранит текст новой заметки.
 
   function addNote() {
-    if (newNoteText.trim()) { 
+    if (newNoteText.trim()) {
       const newNote = {
         id: Date.now(),
-        text: newNoteText,
-        completed: false      //  checkbox
+        text: newNoteText     //  checkbox
       };
-      setNotes([...notes, newNote]);
-      setNewNoteText(''); 
+      setNotes([...notes, newNote]); // обновление заметок 
+      setNewNoteText('');           // обновление текста заметок 
     }
   }
 
   function deleteNote(id) {
     const isConfirmed = window.confirm("Are you want to delete this note?");
     if (isConfirmed) {
-      setNotes(notes.filter(function(note) {
-        return note.id !== id;
-      }));
+      setNotes(
+        notes.filter(
+          function (note) {
+            return note.id !== id;
+          }
+        )
+      );
     }
   }
 
   function toggleNoteCompletion(id) {
-    setNotes(notes.map(function(note) {
-      if (note.id === id) {
-        return { id: note.id, text: note.text, completed: !note.completed };
-      } else {
-        return note;
-      }
-    }));
+    setNotes(
+      notes.map(
+        function (note) {
+          if (note.id === id) {
+            return {
+              id: note.id,
+              text: note.text
+            };
+          }
+          else {
+            return note;
+          }
+        })
+    );
   }
 
   return (
@@ -41,48 +51,52 @@ function App() { // отображение
       <header className="App-header">
         <h1>My Notebook</h1>
       </header>
-      <div className="container-fluid"> 
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6 offset-md-3">
-            <form onSubmit={function(e) { e.preventDefault(); addNote(); }}>
+          <div className="col-md-8 offset-md-2">
+            <form onSubmit={function (e) { // фунция обработчик 
+              e.preventDefault();
+              addNote();
+            }}>
               <div className="form-group">
                 <textarea
                   className="form-control"
-                  placeholder="Enter your note"
-                  value={newNoteText}
-                  onChange={function(e) { setNewNoteText(e.target.value); }}
+                  placeholder="Enter your note" value={newNoteText} //переменнная 
+                  onChange={function (e) { setNewNoteText(e.target.value); }}
                 ></textarea>
               </div>
               <button type="submit" className="btn btn-primary">Add Note</button>
             </form>
           </div>
         </div>
-        <br/>
+        <br />
+        
         <div className="row">
-          <div className="col-md-6 offset-md-3">
+          <div className="col-md-8 offset-md-2">
             <h2>My Notes</h2>
           </div>
         </div>
+
         <div className="row">
-          <div className="notes col-md-6 offset-md-3"> 
+          <div className="notes col-md-8 offset-md-2">
             <ul className="list-group-item">
-              {notes.map(function(note) {
-                return (
-                  <li key={note.id} className="list-group-item">
-                    <div className="checkdiv">
-                      <div className="checkbox-container">
-                        <input
-                          type="checkbox"
-                          checked={note.completed}
-                          onChange={function() { toggleNoteCompletion(note.id); }}
-                        />
-                        <span className={note.completed ? 'completed' : ''}>{note.text}</span>
+              {notes.map(
+                function (note) {
+                  return (
+                    <li className="list-group-item">
+                      <div className="checkdiv">
+                        <div className="checkbox-container">
+                          <input
+                            type="checkbox"
+                            onChange={function () { toggleNoteCompletion(note.id); }}
+                          />
+                          <span>{note.text}</span>
+                        </div>
+                        <button className="btn btn-danger" onClick={function () { deleteNote(note.id); }}>Delete</button>
                       </div>
-                      <button className="btn btn-danger" onClick={function() { deleteNote(note.id); }}>Delete</button>
-                    </div>
-                  </li>
-                );
-              })}
+                    </li>
+                  );
+                })}
             </ul>
           </div>
         </div>
